47ce94ccc1a0e7a535bdeeac037c9e5a80bc5055
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-09-24 09:22:48 +0200

    feat: changed var name from employedSince to seniority

--- src/humanResorces/Employee.java
+++ src/humanResorces/Employee.java
@@ -7,7 +7,7 @@ public class Employee {
    private String name;
    private String lastName;
    private int personalNumber; // 2082
-   private int employedSince;  // angestellt seit
+   private int seniority;  // angestellt seit
    private String function;  // ma/ einrichter/ schichtfuhrer
    private String department;  // Stanzerei / fremdabteilung / student /
    private String branch;  // CH1 / CH2
